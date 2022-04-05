197ebed4783c8f1d43b2e2779d9510de19e53691
Misael <90279764+MisaelMM07@users.noreply.github.com>
2022-04-05 00:21:38 -0500

    Update connect.php

--- exament2/backend/connect.php
+++ exament2/backend/connect.php
@@ -1,6 +1,10 @@
 <?php
 
-$serverName = "localhost";
+/*$serverName = "localhost";
 $dataBase = "id18475034_examen_iot";
 $userName = "id18475034_examen";
+$password = "User12345678#";*/
+$serverName = "localhost";
+$dataBase = "id18475034_iot_curso_itp";
+$userName = "id18475034_admin";
 $password = "User12345678#";
